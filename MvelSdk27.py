674810706a273f1bf37a525414ef658a7e3ea38d
LuisGabrielLiscanoLovera <ccidbcomputacion12@gmail.com>
2019-05-27 20:21:33 -0400

    Update MvelSdk27.py
    para modificar

--- MvelSdk27.py
+++ MvelSdk27.py
@@ -76,3 +76,103 @@ estado_error()
 obtener_reporteZ()
 obtener_estado()
 cerrar_puerto()
+#txt
+"""
+def obtener_reporteZ():
+    
+    #global printer
+    #reporte = printer.GetZReport()
+    salida=  "Numero Ultimo Reporte Z          : "  + str(9.0).replace(".","").zfill(4)#reporte._numberOfLastZReport) #1 L4
+    salida+= "\nFecha Ultimo Reporte Z           : "+ str(0.0).replace(".","").zfill(6)#reporte._zReportDate)         #2 L6
+    salida+= "\nNumero Ultima Factura            : "+ str(0.0).replace(".","").zfill(8)#reporte._numberOfLastInvoice) #3 L8
+    salida+= "\nFecha Ultima Factura             : "+ str(0.0).replace(".","").zfill(6)#reporte._lastInvoiceDate)#4 L6
+    salida+= "\nHora Ultima Factura              : "+ str(0.0).replace(".","").zfill(4)#reporte._lastInvoiceTime)     #5 L4
+    salida+= "\nVentas Exento                    : "+ str(0.0).replace(".","").zfill(10)#reporte._freeSalesTax)#6 L10      
+    salida+= "\nBase Imponible Ventas IVA G      : "+ str(0.0).replace(".","").zfill(10)#reporte._generalRate1Sale)    #7 L10
+    salida+= "\nImpuesto IVA G                   : "+ str(0.0).replace(".","").zfill(10)#reporte._generalRate1Tax) #8L10   
+    salida+= "\nBase Imponible Ventas IVA R      : "+ str(0.0).replace(".","").zfill(10)#reporte._reducedRate2Sale)#9 L10  
+    salida+= "\nImpuesto IVA R                   : "+ str(0.0).replace(".","").zfill(10)#reporte._reducedRate2Tax) #10 L0
+    salida+= "\nBase Imponible Ventas IVA A      : "+ str(0.0).replace(".","").zfill(10)#reporte._additionalRate3Sal)#11 L10
+    salida+= "\nImpuesto IVA A                   : "+ str(0.0).replace(".","").zfill(10)#reporte._additionalRate3Tax)  #12 L10
+    salida+= "\nNota de Debito Exento            : "+ str(0.0).replace(".","").zfill(10)#reporte._freeTaxDebit)#13 L10
+    salida+= "\nBI IVA G en Nota de Debito       : "+ str(0.0).replace(".","").zfill(10)#reporte._generalRateDebit) #14 L10
+    salida+= "\nImpuesto IVA G en Nota de Debito : "+ str(0.0).replace(".","").zfill(10)#reporte._generalRateTaxDebit)#15 L10
+    salida+= "\nBI IVA R en Nota de Debito       : "+ str(0.0).replace(".","").zfill(10)#reporte._reducedRateDebit)#16 L10
+    salida+= "\nImpuesto IVA R en Nota de Debito : "+ str(0.0).replace(".","").zfill(10)#reporte._reducedRateTaxDebit)#17 L10
+    salida+= "\nBI IVA A en Nota de Debito       : "+ str(0.0).replace(".","").zfill(10)#reporte._additionalRateDebit)#18 L10
+    salida+= "\nImpuesto IVA A en Nota de Debito : "+ str(0.0).replace(".","").zfill(10)#reporte._additionalRateTaxDebit)#19 L10
+    salida+= "\nNota de Credito Exento           : "+ str(0.0).replace(".","").zfill(10)#reporte._freeTaxDevolution)#20 L10
+    print (salida)
+"""
+    Rz= str(reporte._numberOfLastZReport).replace(".","").zfill(4)
+    Rz+=str(reporte._zReportDate).replace("/","").zfill(6)  
+    Rz+=str(reporte._numberOfLastInvoice).replace(".","").zfill(8)  
+    Rz+=str(reporte._lastInvoiceDate).replace("/","").zfill(6)  
+    Rz+=str(reporte._lastInvoiceTime).replace(":","").zfill(4)  
+    Rz+=str(reporte._freeSalesTax).replace(".","").zfill(10) 
+    Rz+=str(reporte._generalRate1Sale).replace(".","").zfill(10) 
+    Rz+=str(reporte._generalRate1Tax).replace(".","").zfill(10) 
+    Rz+=str(reporte._reducedRate2Sale).replace(".","").zfill(10) 
+    Rz+=str(reporte._reducedRate2Tax).replace(".","").zfill(10) 
+    Rz+=str(reporte._additionalRate3Sal).replace(".","").zfill(10) 
+    Rz+=str(reporte._additionalRate3Tax).replace(".","").zfill(10) 
+    Rz+=str(reporte._freeTaxDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._generalRateDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._generalRateTaxDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._reducedRateDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._reducedRateTaxDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._additionalRateDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._additionalRateTaxDebit).replace(".","").zfill(10) 
+    Rz+=str(reporte._freeTaxDevolution).replace(".","").zfill(10) 
+    print ("\n---------------------------------------txt Z---------------------------------------\n")
+    print (Rz)
+    print ("----------------------------length: ---->")
+    print (str(len(Rz)))
+"""
+
+def obtener_estado():
+    #global printer
+    #estado_s1 = printer.GetS1PrinterData()
+    salida= "\nEstatus                             : 01"                                                                                                                #1 L2
+    salida+= "\nNumero Cajero                       : " +str().replace(".","").zfill(2)#estado_s1._cashierNumber)		#2 L2
+    salida+= "\nSubtotal Ventas                     : " +str().replace(".","").zfill(17)#estado_s1._totalDailySales) 		#3 L17
+    salida+= "\nNumero Ultima Factura               : " +str().replace(".","").zfill(8)#estado_s1._lastInvoiceNumber)		#4 L8
+    salida+= "\nCantidad Facturas Hoy               : " +str().replace(".","").zfill(5)#estado_s1._quantityOfInvoicesToday	#5 L5
+    salida+= "\nCantidad de Documentos No Fiscales  : " +str().replace(".","").zfill(5)#estado_s1._quantityNonFiscalDocuments)	#6 L5
+    salida+= "\nCantidad de Reportes Z              : " +str().replace(".","").zfill(4)#estado_s1._dailyClosureCounter)		#7 L4
+    salida+= "\nCantidad de Reportes Fiscales       : " +str().replace(".","").zfill(4)#estado_s1._fiscalReportsCounter) 	#8 L4
+    salida+= "\nNumero de RIF                       : " +str().replace(".","").zfill(11)#estado_s1._rif)                      	#9 L11
+    salida+= "\nNumero de Registro                  : " +str().replace(".","").zfill(10)#estado_s1._registeredMachineNumber)	#10 L10
+    salida+= "\nHora de la Impresora                : " +str().replace(":","").zfill(4)#estado_s1._currentPrinterTime)		#11 L4
+    salida+= "\nFecha de la Impresora               : " +str().replace("/","").zfill(6)#estado_s1._currentPrinterDate)		#12 L6    
+    salida+= "\nC}antidad Notas de Credito Hoy      : " +str().replace(".","").zfill(7)#estado_s1._quantityOfNCToday)		#13 L7
+    salida+= "\nNumero Ultimo Documento No Fiscal   : " +str().replace(".","").zfill(10)#estado_s1._numberNonFiscalDocuments) 	#15 L10 
+    print (salida)
+
+    """
+    sUno= "01"                                                                                                                #1 L2
+    sUno+=str(estado_s1._cashierNumber).replace(".","").zfill(2)#estado_s1._cashierNumber)        2 L2
+    sUno+=str(estado_s1._totalDailySales).replace(".","").zfill(17)#estado_s1._totalDailySales)         3 L17
+    sUno+=str(estado_s1._lastInvoiceNumber).replace(".","").zfill(8)#estado_s1._lastInvoiceNumber)        4 L8
+    sUno+=str(estado_s1._quantityOfInvoicesToday.replace(".","").zfill(5)#estado_s1._quantityOfInvoicesToday   5 L5
+    sUno+=str(estado_s1._quantityNonFiscalDocuments).replace(".","").zfill(5)#estado_s1._quantityNonFiscalDocuments)   6 L5
+    sUno+=str(estado_s1._dailyClosureCounter).replace(".","").zfill(4)#estado_s1._dailyClosureCounter)      7 L4
+    sUno+=str(estado_s1._fiscalReportsCounter).replace(".","").zfill(4)#estado_s1._fiscalReportsCounter)     8 L4
+    sUno+=str(estado_s1._rif).replace(".","").zfill(11)#estado_s1._rif)                     9 L11
+    sUno+=str(estado_s1._registeredMachineNumber).replace(".","").zfill(10)#estado_s1._registeredMachineNumber) 10 L10
+    sUno+=str(estado_s1._currentPrinterTime).replace(":","").zfill(4)#estado_s1._currentPrinterTime)       11 L4
+    sUno+=str(estado_s1._currentPrinterDate).replace("/","").zfill(6)#estado_s1._currentPrinterDate)       12 L6    
+    sUno+=str(estado_s1._quantityOfNCToday).replace(".","").zfill(7)#estado_s1._quantityOfNCToday)        13 L7
+    sUno+=str(estado_s1._numberNonFiscalDocuments).replace(".","").zfill(10)#estado_s1._numberNonFiscalDocuments)15 L10 
+    print ("\n-----------------------------------Txt S1-----------------------------------------------\n")
+    print(sUno)
+    print ("----------------------------------length: ---->")
+    print (str(len(sUno)))
+    """
+
+obtener_reporteZ()
+obtener_estado()
+
+"""
+
+
